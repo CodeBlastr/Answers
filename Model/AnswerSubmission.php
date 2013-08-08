49356aad34e8c41650e984e524ffde0e60d6d61e
Joel Byrnes <joel@razorit.com>
2013-08-08 13:52:30 -0400

    Make AnswerSubmission haveMany AnswerAnswer

--- Model/AnswerSubmission.php
+++ Model/AnswerSubmission.php
@@ -5,4 +5,20 @@ class AnswerSubmission extends AnswersAppModel {
 	
 	public $name = 'AnswerSubmission';
 
+	public $hasMany = array(
+		'AnswerAnswer' => array(
+			'className' => 'Answers.Answer',
+			'foreignKey' => 'answer_submission_id',
+			'dependent' => true,
+			'conditions' => '',
+			'fields' => '',
+			'order' => '',
+			'limit' => '',
+			'offset' => '',
+			'exclusive' => '',
+			'finderQuery' => '',
+			'counterQuery' => ''
+			),
+	);
+
 }
