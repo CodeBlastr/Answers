a7526164e1788fcf0d4c365b97f8cdd6da2eb389
Buildrr LLC <richard@buildrr.com>
2013-10-25 00:30:50 +0000

    Adding CI files

---
+++ Test/Case/Model/AnswerTest.php
@@ -0,0 +1,50 @@
+<?php
+App::uses('Answer', 'Answers.Model');
+
+/**
+ * Answer model test cases
+ *
+ * @package 	answers
+ * @subpackage	answers.tests.cases.models
+ */
+class AnswerTestCase extends CakeTestCase {
+
+/**
+ * Fixtures
+ *
+ * @var array
+ */
+	public $fixtures = array();
+
+/**
+ * Start Test callback
+ *
+ * @param string $method
+ * @return void
+ */
+	public function setUp() {
+		parent::setUp();
+		//$this->Answer = ClassRegistry::init('Answers.Answer');
+	}
+
+/**
+ * End Test callback
+ *
+ * @param string $method
+ * @return void
+ */
+	public function tearDown() {
+		//unset($this->Answer);
+		ClassRegistry::flush();
+		parent::tearDown();
+	}
+
+/**
+ * Test adding a Category 
+ *
+ * @return void
+ */
+	public function testAdd() {
+		$this->assertTrue(true);
+	}
+}
---
+++ Test/Case/allTest.php
@@ -0,0 +1,24 @@
+<?php
+class AllTests extends PHPUnit_Framework_TestSuite {
+
+/**
+ * Suite define the tests for this suite
+ *
+ * @return void
+ */
+	public static function suite() {
+		$suite = new PHPUnit_Framework_TestSuite('All Tests');
+
+		$path = APP_TEST_CASES . DS; // core tests
+		$pluginsPath = APP . 'Plugin' . DS;
+		$testPath = DS . 'Test' . DS . 'Case';
+		$modelPath = $testPath . DS . 'Model';
+		$controllerPath = $testPath . DS . 'Controller';
+
+		$behaviorPath = $modelPath . DS . 'Behavior';
+		
+		$suite->addTestFile($pluginsPath . 'Answers' . $modelPath . DS . 'AnswerTest.php');
+			
+    	return $suite;
+	}
+}
